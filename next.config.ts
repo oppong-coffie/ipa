8435a2aa26aaccc55fc2c477890223fbfddd5e05
oppong-coffie <oppongcoffie27@gmail.com>
2025-09-27 07:00:47 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 // next.config.js
 const nextConfig = {
-    webpack(config) {
-      // Important: this must come BEFORE other svg loaders
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    },
+   
   };
   
   module.exports = nextConfig;
